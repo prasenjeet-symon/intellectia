271f0bcbe0c17c4dace8480490bf53703742d810
prasenjeet-symon <prasenjeetsymon@outlook.com>
2023-09-16 10:46:21 +0530

    fixed #1 , fixed #2 , fixed #3 , fixed #4

--- packages/server/src/api_auth/index.ts
+++ packages/server/src/api_auth/index.ts
@@ -1,9 +1,294 @@
-import { Router } from "express";
+import { Router } from 'express';
+import { v4 } from 'uuid';
+import { Constants, PrismaClientSingleton, generateToken, isMagicTokenValid, jwtExpireDate } from '../utils';
+const rateLimit = require('express-rate-limit');
 
 const router = Router();
 
-router.get("/", (req, res) => {
-    res.send({ message: "Hello World" });
+router.get('/', (req, res) => {
+    res.send({ message: 'Hello from Authentication' });
 });
+
+/**
+ * Authenticate user with email and password
+ */
+router.post('/login', async (req, res) => {
+    if (!('email' in req.body) || !('password' in req.body)) {
+        res.status(400).send({ error: 'Email and password are required' });
+        return;
+    }
+
+    if (!(req.body.email && req.body.password)) {
+        res.status(400).send({ error: 'Email and password are required' });
+        return;
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // check if the user already exit in the database
+    const prisma = PrismaClientSingleton.prisma;
+    const oldUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+        include: {
+            sessions: true,
+        },
+    });
+
+    if (!oldUser) {
+        res.status(401).send({ error: 'Invalid email or password' });
+        return;
+    }
+
+    // check if the password is correct
+    if (oldUser.password !== password) {
+        res.status(401).send({ error: 'Invalid email or password' });
+        return;
+    }
+
+    // generate the JWT token
+    const token = generateToken(email, oldUser.userId, false);
+
+    // check for number of sessions
+    if (oldUser.numberOfSessions === oldUser.sessions.length) {
+        res.status(401).send({ error: 'Too many sessions' });
+        return;
+    }
+
+    // update the number of sessions
+    await prisma.user.update({
+        where: {
+            email: email,
+        },
+        data: {
+            sessions: {
+                createMany: {
+                    data: [
+                        {
+                            expiresAt: jwtExpireDate(),
+                            ipAddress: req.ip,
+                            token: token,
+                            userAgent: req.headers['user-agent'] || '',
+                        },
+                    ],
+                },
+            },
+            numberOfSessions: {
+                increment: 1,
+            },
+        },
+    });
+
+    res.send({ token, isAdmin: false, userId: oldUser.userId, email: oldUser.email });
+});
+
+/**
+ * Signup user with email and password
+ */
+router.post('/signup', async (req, res) => {
+    if (!('email' in req.body) || !('password' in req.body)) {
+        res.status(400).send({ error: 'Email and password are required' });
+        return;
+    }
+
+    if (!(req.body.email && req.body.password)) {
+        res.status(400).send({ error: 'Email and password are required' });
+        return;
+    }
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    // check if the user already exit in the database
+    const prisma = PrismaClientSingleton.prisma;
+    const oldUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+
+    if (oldUser) {
+        res.status(409).send({ error: 'User already exists' });
+        return;
+    }
+
+    // create the new user
+    const newUser = await prisma.user.create({
+        data: {
+            email: email,
+            password: password,
+            userId: v4(),
+        },
+        select: {
+            userId: true,
+            email: true,
+        },
+    });
+
+    // generate the JWT token
+    const token = generateToken(email, newUser.userId, false);
+    res.send({ token, isAdmin: false, userId: newUser.userId, email: newUser.email });
+});
+
+/**
+ * Magic URL creation
+ */
+router.post('/magic', async (req, res) => {
+    if (!('email' in req.body)) {
+        res.status(400).send({ error: 'Email is required' });
+        return;
+    }
+
+    if (!req.body.email) {
+        res.status(400).send({ error: 'Email is required' });
+        return;
+    }
+
+    const email = req.body.email;
+    const magicLinkToken = v4();
+    const magicLink = `${Constants.CLIENT_HOST}/server/auth/magic_login?token=${magicLinkToken}&email=${email}`;
+
+    // check if the user is already associated with the email
+    const prisma = PrismaClientSingleton.prisma;
+    const oldUser = await prisma.user.findUnique({
+        where: {
+            email: email,
+        },
+    });
+
+    if (!oldUser) {
+        // we need to create a new user
+        await prisma.user.create({
+            data: {
+                email: email,
+                userId: v4(),
+                password: v4(),
+                magicLink: {
+                    create: {
+                        linkToken: magicLinkToken,
+                    },
+                },
+            },
+            select: {
+                userId: true,
+                email: true,
+            },
+        });
+    } else {
+        // user already exists
+        // create the new magic link
+        await prisma.user.update({
+            where: {
+                email: email,
+            },
+            data: {
+                magicLink: {
+                    create: {
+                        linkToken: magicLinkToken,
+                    },
+                },
+            },
+        });
+    }
+
+    // TODO : In production mode, send the magic link to the user via email and don't return anything
+    res.send({ magicLink });
+});
+
+/**
+ * Magic URL login
+ */
+router.post(
+    '/magic_login',
+    rateLimit({
+        windowMs: 5 * 60 * 1000, // 5 minutes
+        max: 10,
+        message: 'Too many requests',
+    }),
+    async (req, res) => {
+        // token and email are required
+        if (!('token' in req.body) || !('email' in req.body)) {
+            res.status(400).send({ error: 'Token and email are required' });
+            return;
+        }
+
+        if (!(req.body.token && req.body.email)) {
+            res.status(400).send({ error: 'Token and email are required' });
+            return;
+        }
+
+        const token = req.body.token;
+        const email = req.body.email;
+
+        // check if the user is already associated with the email
+        const prisma = PrismaClientSingleton.prisma;
+        const oldUser = await prisma.user.findUnique({
+            where: {
+                email: email,
+            },
+            include: {
+                magicLink: true,
+                sessions: true,
+            },
+        });
+
+        if (!oldUser) {
+            res.status(401).send({ error: 'Invalid token or email' });
+            return;
+        }
+
+        const tokenFound = oldUser.magicLink.find((link) => link.linkToken === token);
+        if (!tokenFound) {
+            res.status(401).send({ error: 'Invalid token or email' });
+            return;
+        }
+
+        const tokenExpirationTimeInMinutes = +(Constants.MAGIC_LINK_TOKEN_EXPIRATION_TIME || 15); // Set your desired expiration time in minutes
+        const tokenCreationTime = tokenFound.createdAt; // Assuming `createdAt` is the token creation time in your model
+        const isTokenValid = isMagicTokenValid(tokenCreationTime, tokenExpirationTimeInMinutes);
+
+        if (!isTokenValid) {
+            res.status(401).send({ error: 'Invalid token or email' });
+            return;
+        }
+
+        // check for the number of active sessions
+        if (oldUser.numberOfSessions === oldUser.sessions.length) {
+            res.status(401).send({ error: 'Too many sessions' });
+            return;
+        }
+
+        // generate the JWT token
+        const tokenJWT = generateToken(email, oldUser.userId, true);
+        const expirationTime = jwtExpireDate();
+        // update the user sessions
+        await prisma.user.update({
+            where: {
+                email: email,
+            },
+            data: {
+                sessions: {
+                    createMany: {
+                        data: [
+                            {
+                                token: tokenJWT,
+                                expiresAt: expirationTime,
+                                ipAddress: req.ip,
+                                userAgent: req.headers['user-agent'] || '',
+                            },
+                        ],
+                    },
+                },
+                numberOfSessions: {
+                    increment: 1,
+                },
+            },
+        });
+
+        res.send({ token: tokenJWT, isAdmin: false, userId: oldUser.userId, email: oldUser.email });
+    },
+);
 
 export default router;
--- packages/server/src/utils.ts
+++ packages/server/src/utils.ts
@@ -1,32 +1,95 @@
-import { NextFunction, Request, Response } from "express";
+import { PrismaClient } from '@prisma/client';
+import { NextFunction, Request, Response } from 'express';
+
+// Constants class
+export class Constants {
+    public static JWT_SECRET = process.env.JWT_SECRET;
+    public static PORT = process.env.PORT;
+    public static DATABASE_URL = process.env.DATABASE_URL;
+    public static TOKEN_EXPIRATION_TIME = process.env.TOKEN_EXPIRATION_TIME; // in hours
+    public static CLIENT_HOST = process.env.CLIENT_HOST;
+    public static MAGIC_LINK_TOKEN_EXPIRATION_TIME = process.env.MAGIC_LINK_TOKEN_EXPIRATION_TIME; // in minutes
+}
 
 /** Verify the JSON WEB TOKEN */
 export const authenticateUser = (req: Request, res: Response, next: NextFunction) => {
     const JWT_SECRET = process.env.JWT_SECRET;
     if (!JWT_SECRET) {
-      res.status(500).json({ error: "Internal server error" });
-      return;
+        res.status(500).json({ error: 'Internal server error' });
+        return;
     }
-  
-    const jwt = require("jsonwebtoken");
+
+    const jwt = require('jsonwebtoken');
     // Get the authentication token from the request headers, query parameters, or cookies
     // Example: Bearer <token>
-    const token = req.headers.authorization ? req.headers.authorization.split(" ")[1] : req.query.token;
-  
+    const token = req.headers.authorization ? req.headers.authorization.split(' ')[1] : req.query.token;
+
     // Verify and decode the token
     try {
-      // Verify the token using your secret key or public key
-      const decodedToken = jwt.verify(token, JWT_SECRET);
-  
-      // Set the userId and email in the request object
-      res.locals.userId = decodedToken.userId;
-      res.locals.email = decodedToken.email;
-      res.locals.isAdmin = decodedToken.isAdmin;
-      // Move to the next middleware
-      next();
+        // Verify the token using your secret key or public key
+        const decodedToken = jwt.verify(token, JWT_SECRET);
+
+        // Set the userId and email in the request object
+        res.locals.userId = decodedToken.userId;
+        res.locals.email = decodedToken.email;
+        res.locals.isAdmin = decodedToken.isAdmin;
+        // Move to the next middleware
+        next();
     } catch (error) {
-      // Token verification failed
-      res.status(401).json({ error: "Invalid token" });
-      return;
+        // Token verification failed
+        res.status(401).json({ error: 'Invalid token' });
+        return;
     }
-  };
+};
+
+/**
+ *
+ * Prisma client singleton
+ */
+export class PrismaClientSingleton {
+    static #instance: PrismaClient;
+
+    static get prisma() {
+        if (!PrismaClientSingleton.#instance) {
+            PrismaClientSingleton.#instance = new PrismaClient();
+        }
+        return PrismaClientSingleton.#instance;
+    }
+}
+
+/**
+ *
+ * Generate the JWT token given the email , userId, isAdmin
+ *
+ */
+export const generateToken = (email: string, userId: string, isAdmin: boolean) => {
+    const jwt = require('jsonwebtoken');
+    const JWT_SECRET = process.env.JWT_SECRET;
+    if (!JWT_SECRET) {
+        throw new Error('JWT_SECRET not set');
+    }
+
+    return jwt.sign({ userId, email, isAdmin: isAdmin }, JWT_SECRET, { expiresIn: Constants.TOKEN_EXPIRATION_TIME });
+};
+
+export function isMagicTokenValid(creationDate: Date, validityMinutes: number): boolean {
+    const now = new Date();
+    const tokenCreationTime = new Date(creationDate);
+
+    // Calculate the time difference in milliseconds between the current time and token creation time
+    const timeDifferenceInMilliseconds = now.getTime() - tokenCreationTime.getTime();
+
+    // Convert the validity period to milliseconds
+    const validityMilliseconds = validityMinutes * 60 * 1000;
+
+    // Check if the token is still valid
+    return timeDifferenceInMilliseconds <= validityMilliseconds;
+}
+
+export function jwtExpireDate() {
+    const jwtExpireHours = Constants.TOKEN_EXPIRATION_TIME || '20h'; // Set your desired expiration time in hours
+    const hourInteger = parseInt(jwtExpireHours, 10);
+    // add this hours to current time to get the expiration time
+    const expirationTime = new Date(Date.now() + hourInteger * 60 * 60 * 1000);
+    return expirationTime;
+}
