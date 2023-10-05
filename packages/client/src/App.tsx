256000077dc4dae072d98ada0560fdaa7cdff6e1
sandheep45 <budtut143@gmail.com>
2023-10-05 14:44:34 +0530

    added shadcn ui and theme

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,6 +1,7 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import IndexPage from "./pages";
 import UserById from "./pages/UserById";
+import { ThemeProvider } from "./components/shared/ThemeProvider";
 
 const routes = createBrowserRouter([
   {
@@ -14,7 +15,11 @@ const routes = createBrowserRouter([
 ]);
 
 function App() {
-  return <RouterProvider router={routes} />;
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <RouterProvider router={routes} />
+    </ThemeProvider>
+  );
 }
 
 export default App;
---
+++ packages/client/src/hooks/useTheme.ts
@@ -0,0 +1,11 @@
+import { ThemeProviderContext } from "@/components/shared/ThemeProvider";
+import { useContext } from "react";
+
+export const useTheme = () => {
+  const context = useContext(ThemeProviderContext);
+
+  if (context === undefined)
+    throw new Error("useTheme must be used within a ThemeProvider");
+
+  return context;
+};
