71f97aa1d74a94b0c1fe89b3d1974f92732bb100
Shovit <119912892+Virtual4087@users.noreply.github.com>
2023-10-06 16:13:12 +0545

    added apiClient.ts to API folder

---
+++ packages/client/src/api/apiClient.ts
@@ -0,0 +1,59 @@
+import axios, { AxiosInstance, AxiosRequestConfig, AxiosResponse } from 'axios';
+
+export class AxiosClient {
+  private static instance: AxiosClient;
+  public axiosInstance: AxiosInstance;
+
+  private constructor() {
+    this.axiosInstance = axios.create({
+      baseURL: 'https://api.example.com',
+      timeout: 1000,
+      headers: { 'Content-Type': 'application/json' },
+    });
+  }
+
+  public static getInstance(): AxiosClient {
+    if (!AxiosClient.instance) {
+      AxiosClient.instance = new AxiosClient();
+    }
+    return AxiosClient.instance;
+  }
+
+  public async get<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.get(url, config);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async post<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.post(url, data, config);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async put<T>(url: string, data?: any, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.put(url, data, config);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  public async delete<T>(url: string, config?: AxiosRequestConfig): Promise<T> {
+    try {
+      const response: AxiosResponse<T> = await this.axiosInstance.delete(url, config);
+      return response.data;
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+export default AxiosClient;
