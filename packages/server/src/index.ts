748a32583a6a48a0c0588b07c2df10432a3e8d8f
patil-gau <gautam.patil@dhwaniris.com>
2023-10-08 01:04:10 +0530

    Generate Intellectia Topics completed

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,11 +1,15 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import { createServer } from './server';
+import {createIntellectiaTopics} from "./utils";
 dotenv.config(); // Load environment variables from .env
 const port = process.env.PORT || 3000;
 
 const app = createServer();
 
 app.listen(port, () => {
+  //create topics
+  createIntellectiaTopics();
   console.log(`Server is running on port ${port}`);
 });
+
