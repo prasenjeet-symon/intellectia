c22859b80ca473cd39766c0201f7ce64790adf9a
sandheep45 <budtut143@gmail.com>
2023-10-12 11:46:34 +0530

    changed prisma seeder file

--- packages/server/prisma/seed.ts
+++ packages/server/prisma/seed.ts
@@ -1,8 +1,18 @@
-import { createMultipleUsers } from "../src/seeder/utils";
+import { createMultipleUsers } from '../src/seeder/utils';
+import { PrismaClientSingleton } from '../src/utils';
 
 const main = async () => {
-  const newUsers = await createMultipleUsers(14)
-  return newUsers
+    const newUsers = await createMultipleUsers(14);
+    return newUsers;
 };
 
-main().then(console.log).catch(console.log);
+main()
+    .then(async () => {
+        const prisma = PrismaClientSingleton.prisma;
+        await prisma.$disconnect();
+    })
+    .catch(async () => {
+        const prisma = PrismaClientSingleton.prisma;
+        await prisma.$disconnect();
+        process.exit(1);
+    });
